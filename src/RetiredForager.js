7868a7bb33537ff5a8fbb24d1aee6fa15ca7c2f3
Arash <arashabbasi@Arashs-MacBook-Pro.local>
2019-10-28 12:20:11 -0700

    Completes code and passes all tests

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,19 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.forage = function() {
+  var str = 'I am too old, let me play cards instead';
+  return str;
+};
+
+RetiredForagerBee.prototype.gamble = function(goodies) {
+  this.treasureChest.push(goodies);
+}
